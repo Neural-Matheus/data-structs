2f5491149ebfc37c968ff8624a9bd1961dc0850a
Matheus Oliveira <72510366+zackkz@users.noreply.github.com>
2023-10-04 20:32:27 -0300

    Update linkedList.cpp
    Added value update function.

--- linked_list/linkedList.cpp
+++ linked_list/linkedList.cpp
@@ -55,16 +55,31 @@ Node* searchVal(int x, Node *n) {
     }
     return n;
 }
+
+int updateVal(int x, int newV, Node *n) {
+
+    while (n != NULL && n->value != x) {
+        n = n->prox;
+    }
+    
+    if (n == NULL) return 0;
+    else n->value = newV;
+    return 1;
+}
 void menuOp(int op) {
     while(op != -1) {
         if (op == 1) {
             int newValue; matheus::cin >> newValue;
+
             addNewNodeFirst(newValue);
+
             matheus::cout << "New point add!" << matheus::endl;
         } else if (op == 2) {
             Node *auxList = listPoints;
+
             printAllRecursive(auxList);
             printAllIterative();
+
         } else if (op == 3) {
 
             Node *auxList = listPoints;
@@ -79,13 +94,31 @@ void menuOp(int op) {
             } else {
                 matheus::cout << "value not found!" << matheus::endl;
             }
-            
+
+        } else if (op == 4) {
+            Node *auxList = listPoints;
+            matheus::cout << "Enter the node you want to search: " << matheus::endl;
+
+            int x; matheus::cin >> x;
+
+            matheus::cout << "Enter the amount you want to add: " << matheus::endl;
+            int newV; matheus::cin >> newV;
+
+            int result = updateVal(x, newV, auxList);
+
+            if (result == 1) {
+                matheus::cout << "The new value was successfully assigned" << matheus::endl;
+            } else {
+                matheus::cout << "Not found" << matheus::endl;
+
+            }
         } else {
             op = -1;
         }
         matheus::cin >> op;
     }
 }
+
 int main() {
     int op; matheus::cin >> op;
     menuOp(op);
